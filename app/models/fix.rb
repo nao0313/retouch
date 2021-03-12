002256156db6e85f2a2c179115e1dca03cd2e364
nao0313 <78333344+nao0313@users.noreply.github.com>
2021-03-12 22:28:32 +0900

    fixモデルのアソシエーション

--- app/models/fix.rb
+++ app/models/fix.rb
@@ -1,4 +1,15 @@
 class Fix < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
+  with_options presence: true do
+    validates :title
+    validates :comment
+    validates :image
+    validates :user_id
+    validates :genre_id, numericality: { other_than: 1 }
+  end
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :genre
 end
