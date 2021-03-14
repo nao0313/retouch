b6b09dc534e8e0ce207434d5f877eea655064226
nao0313 <78333344+nao0313@users.noreply.github.com>
2021-03-15 00:15:00 +0900

    修正リクエストページの作成

--- app/controllers/fixes_controller.rb
+++ app/controllers/fixes_controller.rb
@@ -1,8 +1,31 @@
 class FixesController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create, :edit]
+
   def index
+    @fixes = Fix.all.order("created_at DESC")
   end
   
   def new
+    @fixes = Fix.new
   end
 
+  def create
+    @fixes = Fix.new(fixes_params)
+    if @fixes.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @fixes = Fix.find(params[:id])
+    redirect_to root_path if @fixes.destroy
+  end
+  
+  private
+
+  def fixes_params
+    params.require(:fix).permit(:image, :title, :comment, :genre_id).merge(user_id: current_user.id)
+  end
 end
