78cc0cb9b3acfb4462ce7bd581557c775eb3c08f
nao0313 <78333344+nao0313@users.noreply.github.com>
2021-03-17 19:08:46 +0900

    MY REQページのページネイション

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def show
-    @fixes = current_user.fixes.order("created_at DESC")
+    @fixes = current_user.fixes.order("created_at DESC").page(params[:page]).per(9)
 
   end
 end
