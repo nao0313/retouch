1cbe694e40721b7600f4399050044c533c5e9c7c
nao0313 <78333344+nao0313@users.noreply.github.com>
2021-03-19 20:12:52 +0900

    画像のデータ形式に関するバリデーション

--- app/models/fix.rb
+++ app/models/fix.rb
@@ -1,6 +1,11 @@
 class Fix < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+  attribute :new_image
+  has_many :messages, dependent: :destroy
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :genre
+
 
   with_options presence: true do
     validates :title
@@ -10,7 +15,14 @@ class Fix < ApplicationRecord
     validates :genre_id, numericality: { other_than: 1 }
   end
 
-  extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :genre
-  has_many :messages
+  validate if: :image do
+    if image.respond_to?(:content_type)
+      unless image.content_type.in?(ALLOWED_CONTENT_TYPES)
+        errors.add(:image, :invalid_image_type)
+      end
+    else
+      errors.add(:image, :invalid)
+    end
+  end
+
 end
