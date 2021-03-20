22e03d6a25dbd13caa0736bc4e09cb37f419c957
nao0313 <78333344+nao0313@users.noreply.github.com>
2021-03-20 16:08:50 +0900

    バリデーションの削除

--- app/models/message.rb
+++ app/models/message.rb
@@ -9,15 +9,7 @@ class Message < ApplicationRecord
     validates :fix_id
   end
 
-  validate if: :image do
-    if avatar.respond_to?(:content_type)
-      unless avatar.content_type.in?(ALLOWED_CONTENT_TYPES)
-        errors.add(:avatar, :invalid_avatar_type)
-      end
-    else
-      errors.add(:avatar, :invalid)
-    end
-  end
+
 
   def was_attached?
     self.avatar.attached?
