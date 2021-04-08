468e59c99ae98633eae6e92abe99b805e5989e15
nao0313 <78333344+nao0313@users.noreply.github.com>
2021-04-08 20:58:33 +0900

    ファクトリーボットの作成

--- spec/factories/fixes.rb
+++ spec/factories/fixes.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
   factory :fix do
-    
+    title   { Faker::Lorem.characters(number: 26) }
+    comment  { Faker::Lorem.characters(number: 50) }
+    genre_id { 2 }
+
+    after(:build) do |fix|
+      fix.image.attach(io: File.open('public/images/img.jpeg'), filename: 'img.jpeg')
+    end
   end
 end
