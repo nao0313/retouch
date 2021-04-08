7b2e3f51121591ef38f13639ed87f6bed64103c1
nao0313 <78333344+nao0313@users.noreply.github.com>
2021-04-08 22:08:16 +0900

    messageモデルのFactoryBotの作成

--- spec/factories/fixes.rb
+++ spec/factories/fixes.rb
@@ -3,7 +3,8 @@ FactoryBot.define do
     title   { Faker::Lorem.characters(number: 26) }
     comment  { Faker::Lorem.characters(number: 50) }
     genre_id { 2 }
-
+    association :user
+    
     after(:build) do |fix|
       fix.image.attach(io: File.open('public/images/img.jpeg'), filename: 'img.jpeg')
     end
--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -1,5 +1,10 @@
 FactoryBot.define do
   factory :message do
-    
+    comment {Faker::Lorem.sentence}
+
+
+    after(:build) do |message|
+      message.avatar.attach(io: File.open('public/images/img.jpeg'), filename: 'img.jpeg')
+    end
   end
 end
